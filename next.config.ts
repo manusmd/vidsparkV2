bd39f41ab75a767e5bb9e4a181c150953121c895
Manuel Schmid <manuel.schmid@js-tut.de>
2025-03-14 00:30:42 +0100

    Fix versions and remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(js|ts|tsx)$/,
-      include: /functions/,
-      use: require.resolve("null-loader"),
-    });
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
